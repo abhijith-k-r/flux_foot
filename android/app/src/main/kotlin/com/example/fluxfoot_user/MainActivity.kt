20b9ed506ca788f3d1b483c2eb735338779754a5
Abhijit <abhijithrono007@gmail.com>
2025-08-25 19:31:31 +0530

    Initial commit of FluxFoot

---
+++ android/app/src/main/kotlin/com/example/fluxfoot_user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fluxfoot_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyAPP());
+}
+
+class MyAPP extends StatelessWidget {
+  const MyAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
